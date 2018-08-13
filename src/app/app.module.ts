03ddf18aecc7f2bfe65f800c47671759a9e379b2
cequella <pedro.oliveira.demolay@gmail.com>
2018-08-12 21:11:10 -0300

    Corrections

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,18 +2,30 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { MatCardModule } from '@angular/material';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 
 import { AppComponent } from './app.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
+import { VariableListComponent } from './variable-list/variable-list.component';
 
 @NgModule({
     declarations: [
 	AppComponent,
-	DashboardComponent
+	DashboardComponent,
+	VariableListComponent
     ],
     imports: [
 	BrowserModule,
-	MatCardModule
+	MatCardModule,
+	MatInputModule,
+	MatFormFieldModule,
+	BrowserAnimationsModule,
+	MatButtonModule,
+	MatIconModule
     ],
     providers: [],
     bootstrap: [AppComponent]
